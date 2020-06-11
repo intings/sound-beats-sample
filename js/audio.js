4d94a8211a640d141f95cee2778bf1a35a3dcf25
intings <54090830+intings@users.noreply.github.com>
2020-06-11 11:35:07 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -21,7 +21,7 @@ function PlaySound(i){
 
 (function () {
 	var drumSounds = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-	var audioLocation = "..\\AudioFiles\\";
+	var audioLocation = "..\\sound-beats-sample\\AudioFiles\\";
 	var fileExtension = ".mp3";
 	var sources = [];
 
